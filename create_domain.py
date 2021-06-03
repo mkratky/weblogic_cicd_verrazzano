44f7509eb4d68efc90946770a92f403831f7e130
Mika Rinne <mika.rinne@oracle.com>
2021-06-03 09:09:14 +0000

    Update create_domain.py

--- create_domain.py
+++ create_domain.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 import os, sys
-readTemplate('$HOME/u01/app/oracle/middleware/wlserver/common/templates/wls/wls.jar')
+
+# Path
+path_1 = '$HOME/u01/app/oracle/middleware/wlserver/common/templates/wls/wls.jar'
+path_2 = '$HOME/u01/app/oracle/middleware/wlserver/user_projects/domains/vtest/'
+
+exp_path_1 = os.path.expandvars(path_1)
+exp_path_2 = os.path.expandvars(path_2)
+
+readTemplate(exp_path_1)
 cd('/Security/base_domain/User/weblogic')
 cmo.setPassword('Welcome1')
 cd('/Server/AdminServer')
 cmo.setName('AdminServer')
 cmo.setListenPort(7001)
-writeDomain('$HOME/u01/app/oracle/middleware/wlserver/user_projects/domains/vtest/')
+writeDomain(exp_path_2)
 closeTemplate()
 exit()
