815b4b1405981e70d7a8b8a7183f17f8918a9c65
mkratky <marek.kraatkyy@gmail.com>
2022-04-01 11:22:15 +0200

    updated new oke in oraseemeocids

--- deploywl.sh
+++ deploywl.sh
@@ -1,3 +1,3 @@
-oci ce cluster create-kubeconfig --cluster-id ocid1.cluster.oc1.eu-frankfurt-1.aaaaaaaaae3tqnbzgjstqzdcme2wkojtgnrdomjumm4tmmzxhc3tkyrygvtg --file $HOME/.kube/config --region eu-frankfurt-1 --token-version 2.0.0 
+oci ce cluster create-kubeconfig --cluster-id ocid1.cluster.oc1.eu-frankfurt-1.aaaaaaaavpti3ep4tnyzpyxqfmvpdeunwy5v4bnruulevi7b7c6cizaeybza --file $HOME/.kube/config --region eu-frankfurt-1 --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT
 kubectl apply -f ${PWD}/vtest-application.yaml
 kubectl apply -f ${PWD}/vtest-components.yaml
