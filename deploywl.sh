34a314cb3215bbe432bf17f1f6f1560e589c57d6
mkratky <marek.kraatkyy@gmail.com>
2023-03-20 20:38:36 +0100

    updated cluster id

--- deploywl.sh
+++ deploywl.sh
@@ -1,3 +1,3 @@
-oci ce cluster create-kubeconfig --cluster-id ocid1.cluster.oc1.eu-frankfurt-1.aaaaaaaavpti3ep4tnyzpyxqfmvpdeunwy5v4bnruulevi7b7c6cizaeybza --file $HOME/.kube/config --region eu-frankfurt-1 --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT
+oci ce cluster create-kubeconfig --cluster-id ocid1.cluster.oc1.eu-frankfurt-1.aaaaaaaam5t6sadpzxwaonq4z4nxibsnigloicbdvpgrcffsbc64aop2qm2a --file $HOME/.kube/config --region eu-frankfurt-1 --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT
 kubectl apply -f ${PWD}/vtest-application.yaml -n vtest
 kubectl apply -f ${PWD}/vtest-components.yaml -n vtest
